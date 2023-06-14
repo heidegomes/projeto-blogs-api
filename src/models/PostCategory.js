da977e40345ad10794dfb2ae38378de2895452f1
heidegomes <heidegc@hotmail.com>
2023-06-14 16:12:50 -0300

    requisito 11 feito!

---
+++ src/models/PostCategory.js
@@ -0,0 +1,29 @@
+const postCategoryModel = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+  },
+    {
+      tableName: 'posts_categories',
+      timestamps: false,
+      underscored: true,
+    });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blog_post',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
+module.exports = postCategoryModel;
